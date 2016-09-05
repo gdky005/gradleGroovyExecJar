03b5cfb96681df0e0fff40da3778953222a6c368
wangqing <wangqing@kaolafm.com>
2016-09-05 17:17:31 +0800

    1. 初始化 用gradle 构建的 groovy 生成 可运行的jar 的Demo.

---
+++ src/main/groovy/gd/W.java
@@ -0,0 +1,11 @@
+package gd;
+
+/**
+ * Created by WangQing on 16/9/5.
+ */
+public class W {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+    }
+}
---
+++ src/main/groovy/gd/WQ.groovy
@@ -0,0 +1,11 @@
+package gd
+/**
+ * Created by WangQing on 16/9/5.
+ */
+
+
+//println("hello1")
+
+String relativelyPath=System.getProperty("user.dir");
+
+println relativelyPath
